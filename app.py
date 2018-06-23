4c1d2f89b9128bd5871fa3d7301916ff95f220f7
Mathieu Frenette <Silphid@users.noreply.github.com>
2018-06-23 12:42:33 -0400

    Fixed for Python 3

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
-from os import curdir,sep
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 PORT_NUMBER = 8080
 
@@ -9,17 +8,15 @@ class myHandler(BaseHTTPRequestHandler):
 	#Handler for the GET requests
 	def do_GET(self):
 		self.send_response(200)
-		self.send_header('Content-type','image/png')
+		self.send_header('Content-type', 'image/png')
 		self.end_headers()
-		f = open(curdir + sep + 'logo.png')
+		f = open('logo.png', 'rb')
 		self.wfile.write(f.read())
-		return
 
 try:
 	server = HTTPServer(('', PORT_NUMBER), myHandler)
-	print('Started httpserver on port ' + PORT_NUMBER)
+	print('Started httpserver on port' , PORT_NUMBER)
 	server.serve_forever()
 
 except KeyboardInterrupt:
 	server.socket.close()
-	
